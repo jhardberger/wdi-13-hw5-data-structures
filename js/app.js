d8c69051ebf6780a8119cce6b8ab123e698d17f7
John <john@Johns-MacBook-Pro.local>
2018-09-18 07:37:14 -0500

    Commit 1: questions

---
+++ js/app.js
@@ -0,0 +1,61 @@
+
+
+//1.
+
+// light switch --> an object containing a boolean
+
+const lightSwitch = {
+	lightOn: true
+	//you could also build a function to turn the light on and off
+};
+
+// email address --> string
+
+const myEmail = "john.hardberge@gmail.com";
+
+//spaceship --> an object with a variety of properties
+
+const deathStar = {
+	exterior: 'hull',
+	weapons: ['laser blasters', 'tractor beam'],
+	drive: 'warp'
+};
+
+//list of student names --> an array
+
+const saltySardinesChicago = ['john', 'ashley', 'greg', 'fil', 'josh', 'andy', 'charlotte', 'chrisL', 'chrisR'];
+
+//list of student names with location --> array of objects
+
+const saltySardinesFinder = [
+	{
+		name: 'john',
+		location: 'home'
+	},
+	{
+		name: 'ashley',
+		location: 'madison, WI'
+	},
+	{
+		name: 'fil',
+		locatoin: 'the burbs'
+	}
+];
+
+//list of student names + location + tv --> array of objects again, with one property of the object being an array
+
+const saltySardinesTVFinder = [
+	{
+		name: 'john',
+		location: 'home',
+		favoriteShows: ['Joe Pera Talks to You', 'The Good Place', 'Jane the Virgin', 'Mad Men']
+	},
+	{
+		name: 'fil',
+		location: 'the burbs',
+		favoriteShows: ['Mythbusters']
+	}
+];
+
+
+
